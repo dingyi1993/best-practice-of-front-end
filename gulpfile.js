a7f480b0c2dae622087666590bd9a6768722fe99
dingyi1993 <781745492@qq.com>
2016-12-08 21:04:45 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,25 @@
 var gulp = require('gulp');
 var del = require('del');
 var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence').use(gulp);
+
+var config = require('./config/gulp');
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: config.root
         },
         port: 2333,
-        startPath: 'dist/index.html'
+        startPath: 'index.html'
     })
+});
+gulp.task('clean', function(callback) {
+    del(config.root + '**/*', callback);
 });
 gulp.task('html', function() {
     return gulp.src('src/*.html')
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(config.root));
 });
-gulp.task('default', ['html', 'browser-sync'], function() {
-
+gulp.task('default', function(callback) {
+    runSequence(['clean', 'html', 'browser-sync'], callback);
 });
