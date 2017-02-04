1def106b43a7e92e98728d42730ebdbdcbf39674
丁一 <yi_ding1@kingdee.com>
2017-02-04 18:06:33 +0800

    Add sass lint.

--- gulp/task/sass.js
+++ gulp/task/sass.js
@@ -2,9 +2,11 @@ var rev = require('../util/rev');
 
 module.exports = function(gulp) {
     var postCssPlugins = [
+        // plugins.stylelint(),
         plugins.autoprefixer({browsers: ['last 9 versions'], cascade: false}),
         plugins.postcssPx2rem({remUnit: 75})
     ];
+    // console.log(plugins);
 
     gulp.task('sass', function() {
         return gulp.src(config.src.file.sass)
@@ -13,14 +15,25 @@ module.exports = function(gulp) {
 
             .pipe(plugins.debug({title: '[sass]编译:'}))
 
-            .pipe(plugins.sass())
-
-            .pipe(plugins.postcss(postCssPlugins))
+            // .pipe(plugins.stylelint({
+            //     reporters: [
+            //         {formatter: 'string', console: true}
+            //     ],
+            //     debug: true
+            // }))
 
             .on('error', plugins.notify.onError({
                 title: 'compile error',
                 message: '<%= error.message %>'
             }))
+
+            .pipe(plugins.sassLint())
+
+            .pipe(plugins.sassLint.format())
+
+            .pipe(plugins.sass())
+
+            .pipe(plugins.postcss(postCssPlugins))
 
             .pipe(plugins.if(config.isProduction, plugins.cleanCss({debug: true}, function(details) {
                 common.log('min css: ' + details.name + ': ' + details.stats.originalSize + ' => ' + details.stats.minifiedSize);
